698610387f1ab05aac94f7558e11d607b6bbb4bf
Prince Thakur <prince787640@gmail.com>
2024-09-22 01:02:09 +0530

    Revert something

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 const age = 24;
 const name1 = "prince thakur ";
 const mobile = 7876403701;
@@ -11,3 +12,6 @@ const cass = "college"
 function isabove18(age){
     return age >= 18
 }
+=======
+const age = 24
+>>>>>>> parent of ec30a02 (initial code added)
--- utils/util.js
+++
@@ -1,6 +0,0 @@
-function add(a,b){
-    return sum = a + b;
-}
-
-const addition = add(4,5);
-console.log(addition);
